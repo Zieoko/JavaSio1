2fc399ced355e82bca4366a6e0ec7f269a2b6eb2
Zieoko_ <remipernak@gmail.com>
2024-11-08 14:15:51 +0100

    2

--- SIO1/src/jeu_de_l_oie.java
+++ SIO1/src/jeu_de_l_oie.java
@@ -6,24 +6,32 @@ public class jeu_de_l_oie {
     public static void main(String[] args) {
 
         int caseObjectif = 20;
-        int caseCourante = 0;
         int nbLancers = 5;
-        Random rnd = new Random(7);
+        int essai = 0;
+        boolean win = false ;
+        Random rnd = new Random();
 
         System.out.println("Bienvenue ! Commençons...");
 
-        for (int i = 1; i <= nbLancers; i++) {
-            int lancer = rnd.nextInt(6) +1;
-            caseCourante = caseCourante + lancer;
-            System.out.println(String.format("Lancer %d : vous avez fait %d. Vous êtes sur la case %d", lancer, caseCourante));
-        }
+        while (win == false){
 
-        if (caseCourante == caseObjectif){
-            System.out.println("Vous avez gagné !");
-        } else if (caseCourante > caseObjectif) {
-            System.out.println("Vous dépassez, vous avez perdu !");
-        } else {
-            System.out.println("Il vous en manque pour arriver à " + caseObjectif +" !");
+            essai += 1;
+            int caseCourante = 0;
+
+            for (int i = 1; i <= nbLancers; i++) {
+                int lancer = rnd.nextInt(6) +1;
+                caseCourante = caseCourante + lancer;
+                System.out.println(String.format("Lancer %d : vous avez fait %d. Vous êtes sur la case %d",i , lancer, caseCourante));
+            }
+            if (caseCourante == caseObjectif){
+                System.out.println("Vous avez gagné !");
+                win = true;
+            } else if (caseCourante > caseObjectif) {
+                System.out.println("Vous dépassez, vous avez perdu !");
+            } else {
+                System.out.println("Il vous en manque pour arriver à " + caseObjectif +" !");
+            }
         }
+        System.out.println("Nombre d'essai(s) "+ essai +" !");
     }
 }
